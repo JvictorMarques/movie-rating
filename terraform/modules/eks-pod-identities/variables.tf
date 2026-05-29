1ce1525f7a57e956c0d9645e38ba626175b8208d
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-05-29 19:35:13 -0300

    refactor(terraform): remove eks-pod-identities module
    - Replaced by eks-pod-identity-roles module with cleaner structure

--- terraform/modules/eks-pod-identities/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  description = "The name of the project"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "The name of the EKS cluster"
-  type        = string
-}
