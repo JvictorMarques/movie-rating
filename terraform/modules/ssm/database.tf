31253702ed0a64aa75b440c9a327ae914b40ad74
João Victor <114443824+JvictorMarques@users.noreply.github.com>
2026-06-02 17:54:12 -0300

    feat(ci): add terraform plan step
    * feat(ci): add terraform plan step
    
    * fix(ci): add permissions for contents and id-token in CI workflow
    
    * fix(ci): add permissions for id-token in terraform job
    
    * fix(ci): add AWS_ROLE_TO_ASSUME secret for Terraform jobs
    
    * feat(ci): add Terraform plan step and PR comment functionality
    
    * fix(ci): update permissions for pull requests and upgrade GitHub script action version
    
    * fix(ci): remove env AWS_REGION from CI workflow

--- terraform/modules/ssm/database.tf
+++ terraform/modules/ssm/database.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "database" {
   for_each = local.database_secrets_keys
 
   name        = "/${var.project_name}/database/${each.key}"
-  description = var.database_secrets[each.key].description
+  description = nonsensitive(var.database_secrets[each.key].description)
   type        = "SecureString"
   value       = var.database_secrets[each.key].value
 }
